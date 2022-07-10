d39e9c384a3a04079b3709504e278f240cf8ae5e
Prabesh <prabesh.bista369@gmail.com>
2022-07-10 11:13:01 +0545

    designed detail page

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
-    <div className='h-[60vh] w-[60%] bg-black m-0 p-0'>
+    <div className='h-[90vh] w-[40%] bg-white shadow-md shadow-[#0000008f] mt-10 p-0 flex mb-10 flex-col'>
+      <h1 className='font-bold text-2xl text-center'>{props.title}</h1>
+      <img src={props.imgSrc} className="h-[40%] w-[100%] object-cover mt-10"/>
+      <p className='text-md font-bold text-center mt-5'>{props.address}</p>
+      <p className='text-sm font-bold text-center mt-10'>{props.description}</p>
 
-    </div>
+  </div>
   )
 }
 
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,36 @@
 import MeetupDetails from "../../components/meetups/MeetupDetails";
+import { useRouter } from "next/dist/client/router";
 
-const MeetupDetail=()=>{
-    return <MeetupDetails/>
-}
+
+const DummyMeetups=[
+    {
+        id:"m1",
+        title:"React Meetup",
+        image:"https://media.istockphoto.com/photos/architecture-of-lalitpur-metropolitan-city-the-third-largest-city-of-picture-id1013294000?s=2048x2048",
+        address:"Bojepokhari,Imadole",
+        description:"This is a first meetup!"
+    },
+    {
+        id:"m2",
+        title:"React ",
+        image:"https://media.istockphoto.com/photos/architecture-of-lalitpur-metropolitan-city-the-third-largest-city-of-picture-id1013294000?s=2048x2048",
+        address:"Bojepokhari,Imadole",
+        description:"This is a first meetup!"
+    }
+]
+
+const MeetupDetail = () => {
+    const router=useRouter();
+
+    const meetup=DummyMeetups.find((meetup)=>meetup.id===router.query.meetupId);
+  return (
+    <MeetupDetails
+      imgSrc={meetup.image}
+      title={meetup.title}
+      address={meetup.address}
+      description={meetup.description}
+    />
+  );
+};
 
 export default MeetupDetail;
