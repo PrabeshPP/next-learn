1022e746042dd88b5b58e54ff2b616543ae4dd1b
Prabesh <prabesh.bista369@gmail.com>
2022-07-11 23:19:23 +0545

    final commit

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -4,7 +4,7 @@ const MeetupDetails = (props) => {
   return (
     <div className='h-[90vh] w-[40%] bg-white shadow-md shadow-[#0000008f] mt-10 p-0 flex mb-10 flex-col'>
       <h1 className='font-bold text-2xl text-center'>{props.title}</h1>
-      <img src={props.imgSrc} className="h-[40%] w-[100%] object-cover mt-10"/>
+      <Image src={props.imgSrc} className="h-[40%] w-[100%] object-cover mt-10" alt={props.title}/>
       <p className='text-md font-bold text-center mt-5'>{props.address}</p>
       <p className='text-sm font-bold text-center mt-10'>{props.description}</p>
 
